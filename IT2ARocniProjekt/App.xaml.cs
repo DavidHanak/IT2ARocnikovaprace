f749b60e38c8cf84868b9c40f7f1acd0600a90fd
DavidHanak <61800hanak@student.sstebrno.cz>
2026-04-17 14:23:34 +0200

    Add files via upload

---
+++ IT2ARocniProjekt/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace IT2ARocniProjekt
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ IT2ARocniProjekt/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace IT2ARocniProjekt
+{
+    public partial class MainWindow : Window
+    {
+        private bool wokenUp = false;
+        private bool hasPistol = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ShowMessage("Probudil ses v posteli po divoké noci v saloonu, šerife.\nNajdi svou zbran.");
+        }
+
+        private void ShowMessage(string text)
+        {
+            message.Text = text;
+        }
+
+        private void Bed_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (!wokenUp)
+            {
+                wokenUp = true;
+                bed.Opacity = 0.4;
+                ShowMessage("Dobré ráno, šerife! \nTed najdi svou zbran...");
+            }
+        }
+
+        private void Pistol_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (!hasPistol)
+            {
+                hasPistol = true;
+                invPistol.Text = "🔫";
+                pistol.Opacity = 0.3;
+                pistol.IsHitTestVisible = false;
+                ShowMessage("Našel jsi svou starou věrnou pistoli!\n\n");
+            }
+        }
+    }
+}
